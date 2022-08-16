a8eef219f2cc36792b1b025cdec6dd61a4854bce
theonefx.chenx <theonefx.chenx@alibaba-inc.com>
2022-08-16 11:09:36 +0800

    use label name when sync device to edgex
    Signed-off-by: theonefx.chenx <theonefx.chenx@alibaba-inc.com>

--- pkg/clients/edgex-foundry/util.go
+++ pkg/clients/edgex-foundry/util.go
@@ -44,10 +44,15 @@ type ClientURL struct {
 	Port int
 }
 
-func getEdgeDeviceName(d *devicev1alpha1.Device) string {
+type EdgexNameProvider interface {
+	GetName() string
+	GetObjectMeta() metav1.Object
+}
+
+func getEdgexName(d EdgexNameProvider) string {
 	var actualDeviceName string
-	if _, ok := d.ObjectMeta.Labels[EdgeXObjectName]; ok {
-		actualDeviceName = d.ObjectMeta.Labels[EdgeXObjectName]
+	if _, ok := d.GetObjectMeta().GetLabels()[EdgeXObjectName]; ok {
+		actualDeviceName = d.GetObjectMeta().GetLabels()[EdgeXObjectName]
 	} else {
 		actualDeviceName = d.GetName()
 	}
@@ -57,7 +62,7 @@ func getEdgeDeviceName(d *devicev1alpha1.Device) string {
 func toEdgexDeviceService(ds *devicev1alpha1.DeviceService) dtos.DeviceService {
 	return dtos.DeviceService{
 		Description:   ds.Spec.Description,
-		Name:          ds.GetName(),
+		Name:          getEdgexName(ds),
 		LastConnected: ds.Status.LastConnected,
 		LastReported:  ds.Status.LastReported,
 		Labels:        ds.Spec.Labels,
@@ -134,7 +139,7 @@ func toKubeDeviceService(ds dtos.DeviceService) devicev1alpha1.DeviceService {
 func toEdgeXDevice(d *devicev1alpha1.Device) dtos.Device {
 	md := dtos.Device{
 		Description:    d.Spec.Description,
-		Name:           d.GetName(),
+		Name:           getEdgexName(d),
 		AdminState:     string(toEdgeXAdminState(d.Spec.AdminState)),
 		OperatingState: string(toEdgeXOperatingState(d.Spec.OperatingState)),
 		Protocols:      toEdgeXProtocols(d.Spec.Protocols),
@@ -357,7 +362,7 @@ func toKubeProfileProperty(rp dtos.ResourceProperties) devicev1alpha1.ResourcePr
 func toEdgeXDeviceProfile(dp *devicev1alpha1.DeviceProfile) dtos.DeviceProfile {
 	return dtos.DeviceProfile{
 		Description:     dp.Spec.Description,
-		Name:            dp.GetName(),
+		Name:            getEdgexName(dp),
 		Manufacturer:    dp.Spec.Manufacturer,
 		Model:           dp.Spec.Model,
 		Labels:          dp.Spec.Labels,
